2237d33f5cf45e1d774732da45c1c8555cf68742
Carlo Andaya <2507017+carloandaya@users.noreply.github.com>
2019-12-18 13:12:20 -0800

    Add email to DimEmployee

--- MyWirelessDW/DimEmployee.sql
+++ MyWirelessDW/DimEmployee.sql
@@ -2,7 +2,8 @@
 (
 	[EmployeeKey] INT NOT NULL PRIMARY KEY IDENTITY, 
     [EmployeeName] NVARCHAR(50) NOT NULL, 
-    [ATTUID] NVARCHAR(10) NULL
+    [ATTUID] NVARCHAR(10) NULL, 
+    [Email] NVARCHAR(50) NULL
 )
 
 GO
