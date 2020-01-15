332dfe46f4a3653ef9caf02ab11640d518084c12
Carlo Andaya <2507017+carloandaya@users.noreply.github.com>
2020-01-15 14:38:08 -0800

    Add field for Employee ID (EID).

--- MyWirelessDW/DimEmployee.sql
+++ MyWirelessDW/DimEmployee.sql
@@ -1,9 +1,15 @@
 ﻿CREATE TABLE [dbo].[DimEmployee]
 (
-	[EmployeeKey] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [EmployeeName] NVARCHAR(50) NOT NULL, 
+	[EmployeeKey] INT NOT NULL PRIMARY KEY, 
+    [EmployeeName] NVARCHAR(50) NOT NULL,
+	[EID] INT NOT NULL IDENTITY(150000, 1),
     [ATTUID] NVARCHAR(10) NULL, 
-    [Email] NVARCHAR(50) NULL
+    [Email] NVARCHAR(50) NULL, 
+    [IsEmailCreated] BIT NULL DEFAULT 0
 )
 
 GO
+
+
+CREATE UNIQUE INDEX [IX_DimEmployee_ATTUID] ON [dbo].[DimEmployee] ([ATTUID])
+WHERE [ATTUID] IS NOT NULL
